5246d22c326215a8cb07195fa9ccfd4d96c3441d
mo_behairy <m.buhairi@team.wesal.com.sa>
2023-05-28 11:51:31 +0300

    first commit

---
+++ src/components/HelpComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="help_page">
+    <h3 class="text-center">يجب قراءة التعليمات</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed minus maxime voluptatibus modi expedita molestiae amet esse fuga dolores, deleniti corporis magni placeat culpa quasi earum aliquam quod sequi aspernatur ducimus molestias soluta possimus autem voluptates rem? Debitis officia, blanditiis distinctio facilis animi, consequuntur porro aperiam eum fugit sed voluptates accusantium eveniet labore libero maxime quod. Incidunt expedita nesciunt harum id, voluptatibus ab blanditiis repellendus atque odio quasi impedit libero aspernatur dolorum eveniet porro vitae repellat exercitationem nam modi omnis facere ad aperiam magni? Reiciendis corrupti quidem possimus, asperiores atque alias, in officiis a expedita, nulla nostrum consequatur dignissimos autem odit! Illo, quasi numquam? Quidem temporibus ipsam voluptatibus suscipit libero doloribus obcaecati eum corporis voluptatem dignissimos, cum laudantium, reprehenderit adipisci.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelpComponent',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HelpView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Help">
+    <h1 class="text-danger">Help page Content</h1>
+  </div>
+</template>
+
+<script>
+
+// import HelpComponent from '@/components/HelpComponent.vue'
+
+// export default {
+//   name: 'HelpComponent',
+//   components: {
+//     HelpComponent
+//   }
+// }
+
+</script>
